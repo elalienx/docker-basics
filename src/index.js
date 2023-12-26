9fb0fef918ed6c9608f28b814ec9ec23999e3f1c
Eduardo <el_alienx@hotmail.com>
2023-12-26 15:42:07 +0100

    added the express server

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@
 const app = require("express")();
 
 // Properties
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8000;
 
 app.get("/", (request, response) =>
   response.json({ message: "Docker is easy 🐳" })
 );
 
 app.listen(port, () => console.log(`app listening on localhost:${port}`));
+    
